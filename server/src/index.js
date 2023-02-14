4fdf767862669f2f33ceeff60f22105718ad5615
AryanKarim <aryanmajid97@gmail.com>
2023-02-14 23:40:11 +0300

    add apollo server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-//TODO
+// npm install @apollo/server graphql
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+
+const server = new ApolloServer({ typeDefs });
+
+const { url } = await startStandaloneServer(server, {
+  context: async ({ req }) => ({ token: req.headers.token }),
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at ${url}`);
